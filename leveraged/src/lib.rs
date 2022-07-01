30f3eb5cfea4a76b12b116a9723f9da097f230a7
bonedaddy <boned4ddy@protonmail.com>
2022-07-01 13:42:55 -0700

    wip leveraged yield farming ex

---
+++ leveraged/src/lib.rs
@@ -0,0 +1,7 @@
+pub mod accounts;
+
+use anchor_lang::{solana_program, solana_program::pubkey::Pubkey};
+
+use static_pubkey::static_pubkey;
+
+pub const ID: Pubkey = static_pubkey!("Bt2WPMmbwHPk36i4CRucNDyLcmoGdC7xEdrVuxgJaNE6");
