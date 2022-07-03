50699aa6baf893cb785093d220571bda9e2a5c65
bonedaddy <boned4ddy@protonmail.com>
2022-07-02 22:32:09 -0700

    add remaining accounts to withdraw_orca_vault_dd_close

--- leveraged/src/instructions/withdraw_orca_vault_dd_close.rs
+++ leveraged/src/instructions/withdraw_orca_vault_dd_close.rs
@@ -50,6 +50,11 @@ pub struct WithdrawOrcaVaultDoubleDip<'info> {
 
 pub fn withdraw_orca_vault_dd_close<'info>(
     accounts: WithdrawOrcaVaultDoubleDip<'info>,
+    lending_market_account: &AccountInfo<'info>,
+    user_farm_obligation: &AccountInfo<'info>,
+    lending_market_authority: &AccountInfo<'info>,
+    lending_program: &AccountInfo<'info>,
+    position_info_account: &AccountInfo<'info>,
     obligation_index: u8,
     withdraw_percent: u8,
     close_method: u8,
@@ -60,9 +65,18 @@ pub fn withdraw_orca_vault_dd_close<'info>(
     ix_data.extend_from_slice(&AnchorSerialize::try_to_vec(&obligation_index).unwrap());
     ix_data.extend_from_slice(&AnchorSerialize::try_to_vec(&withdraw_percent).unwrap());
     ix_data.extend_from_slice(&AnchorSerialize::try_to_vec(&close_method).unwrap());
+
+
+    let mut accounts = accounts.to_account_metas(None);
+    accounts.push(AccountMeta::new_readonly(lending_market_account.key(), false));
+    accounts.push(AccountMeta::new(user_farm_obligation.key(), false));
+    accounts.push(AccountMeta::new_readonly(lending_market_authority.key(), false));
+    accounts.push(AccountMeta::new_readonly(lending_program.key(), false));
+    accounts.push(AccountMeta::new(position_info_account.key(), false));
+
     Some(Instruction {
         program_id: crate::ID,
-        accounts: accounts.to_account_metas(None),
+        accounts: accounts,
         data: ix_data,
     })
 }
