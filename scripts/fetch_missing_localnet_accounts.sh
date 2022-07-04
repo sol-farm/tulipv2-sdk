275f6fb1579c0af57c9ca25fb8eb5e081f4123af
bonedaddy <boned4ddy@protonmail.com>
2022-07-04 14:58:02 -0700

    update matching parameter

--- scripts/fetch_missing_localnet_accounts.sh
+++ scripts/fetch_missing_localnet_accounts.sh
@@ -3,4 +3,4 @@
 # finds any "missing" accounts from the localnet environment, namely by looking for configurations
 # in Anchor.toml which specify localnet accounts to clone during runtime.
 
-grep "test\.validator\.clone" -A 1 examples/Anchor.toml | grep "address = .*" | awk -F '=' '{print $2}' | tr -d '" ' | tee accounts.txt
+grep "test\.validator\.account" -A 1 examples/Anchor.toml | grep "address = .*" | awk -F '=' '{print $2}' | tr -d '" ' | tee accounts.txt
