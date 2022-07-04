a8f3efd4b7d918718d8568a9b7aafe4aee7d6d4a
bonedaddy <boned4ddy@protonmail.com>
2022-07-04 14:57:02 -0700

    add scripts for maintaining anchor toml accounts file

---
+++ scripts/fetch_missing_localnet_accounts.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+# finds any "missing" accounts from the localnet environment, namely by looking for configurations
+# in Anchor.toml which specify localnet accounts to clone during runtime.
+
+grep "test\.validator\.clone" -A 1 examples/Anchor.toml | grep "address = .*" | awk -F '=' '{print $2}' | tr -d '" ' | tee accounts.txt
